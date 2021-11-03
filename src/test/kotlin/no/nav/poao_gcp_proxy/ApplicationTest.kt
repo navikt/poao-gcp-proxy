29ac8a67f25829e935782d80c613c5d5f8472478
Alexander Gård <alex.gaard@hotmail.com>
2021-11-03 11:13:36 +0100

    Fikset app boostrap test

--- src/test/kotlin/no/nav/poao_gcp_proxy/ApplicationTest.kt
+++ src/test/kotlin/no/nav/poao_gcp_proxy/ApplicationTest.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 
 @ActiveProfiles("test")
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class ApplicationTest {
 
 	@Test
