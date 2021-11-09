16827cdadc50c0ab234f8bae83cfc321b0522fea
Alexander Gård <AlexGaard@users.noreply.github.com>
2021-11-09 16:54:03 +0100

    Fix token validering (#1)
    * WIP: Startet på fiksing av token validering
    
    * Bumpet versjoner

--- src/test/kotlin/no/nav/poao_gcp_proxy/config/TestApplicationConfig.kt
+++ src/test/kotlin/no/nav/poao_gcp_proxy/config/TestApplicationConfig.kt
@@ -2,15 +2,22 @@ package no.nav.poao_gcp_proxy.config
 
 import no.nav.poao_gcp_proxy.proxy_filter.PreRequestZuulFilter
 import no.nav.poao_gcp_proxy.token_provider.ScopedTokenProvider
+import no.nav.security.token.support.core.configuration.MultiIssuerConfiguration
+import no.nav.security.token.support.core.configuration.ProxyAwareResourceRetriever
+import no.nav.security.token.support.core.validation.JwtTokenValidationHandler
+import no.nav.security.token.support.spring.MultiIssuerProperties
+import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 
+
 @Profile("test")
 @Configuration
 @EnableZuulProxy
+@EnableJwtTokenValidation
 @EnableConfigurationProperties(ProxyConfig::class)
 class TestApplicationConfig {
 
@@ -24,8 +31,17 @@ class TestApplicationConfig {
 	}
 
 	@Bean
-	fun preRequestZuulFilter(proxyConfig: ProxyConfig, scopedTokenProvider: ScopedTokenProvider): PreRequestZuulFilter {
-		return PreRequestZuulFilter("/proxy", proxyConfig, scopedTokenProvider)
+	fun preRequestZuulFilter(
+		proxyConfig: ProxyConfig,
+		scopedTokenProvider: ScopedTokenProvider,
+		jwtTokenValidationHandler: JwtTokenValidationHandler
+	): PreRequestZuulFilter {
+		return PreRequestZuulFilter("/proxy", proxyConfig, scopedTokenProvider, jwtTokenValidationHandler)
+	}
+
+	@Bean
+	fun jwtTokenValidationHandler(config: MultiIssuerConfiguration): JwtTokenValidationHandler {
+		return JwtTokenValidationHandler(config)
 	}
 
 }
